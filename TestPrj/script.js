c8dc7ad3c68a3cd7e3e28f8be6030378f3018589
Son Nguyen <sonbeo1907@gmail.com>
2026-01-12 16:10:02 +0700

    added js test

---
+++ TestPrj/script.js
@@ -0,0 +1,16 @@
+function showMessage(){
+     alert('Hello!');
+}
+
+function showHiddenMessage(){
+     document.getElementById('messageDiv').innerHTML = 'Hello!';
+ }
+
+function rewritePage(){
+     document.write('Cleaned!');
+
+}
+
+ function logger(){
+     console.log('Logged');
+ }
