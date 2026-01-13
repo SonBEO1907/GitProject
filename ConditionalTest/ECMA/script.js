5bc7eabe05bc74517dfe190d124a063d875c20a4
Son Nguyen <sonbeo1907@gmail.com>
2026-01-13 14:25:44 +0700

    added ECMA

---
+++ ConditionalTest/ECMA/script.js
@@ -0,0 +1,10 @@
+function main(){
+    let value = prompt('What is the "official" name of JavaScript?', '');
+    if (value == 'ECMAScript') {
+        alert('Right!');
+    } else {
+        alert("You don't know? ECMAScript!");
+    }
+}
+
+main();
