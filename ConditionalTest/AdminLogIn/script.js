21761226414ba223ad482e4998f4c4ead47b3900
Son Nguyen <sonbeo1907@gmail.com>
2026-01-13 14:34:14 +0700

    login added

---
+++ ConditionalTest/AdminLogIn/script.js
@@ -0,0 +1,19 @@
+function main(){
+    let userName = prompt("Who's there?", '');
+    if (userName === 'Admin') {
+        let pass = prompt('Password?', '');
+        if (pass === 'TheMaster') {
+    	    alert('Welcome!');
+        } else if (pass === '') {
+            alert('Canceled.');
+        } else {
+            alert('Wrong password');
+        }
+    } else if (userName === '') {
+        alert('Canceled');
+    } else {
+        alert("I don't know you");
+    }
+}
+
+main();
