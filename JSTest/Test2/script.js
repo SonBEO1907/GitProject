5776de926984ee57cfb08ec9ea70f69a112420a3
Son Nguyen <sonbeo1907@gmail.com>
2026-01-13 10:31:35 +0700

    added 2nd test

---
+++ JSTest/Test2/script.js
@@ -0,0 +1,6 @@
+inputHeight = prompt("Enter the height");
+inputWidth = prompt("Enter the width");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+alert("The area is: " + area);
